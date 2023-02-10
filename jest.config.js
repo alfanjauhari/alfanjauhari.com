f4d732a62c05233cff0a9a4a29275dba4bc8c6ba
Restyled.io <commits@restyled.io>
2023-02-10 12:24:31 +0000

    Restyled by clang-format

--- jest.config.js
+++ jest.config.js
@@ -2,17 +2,17 @@
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  dir: './',
+  dir : './',
 });
 
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  moduleNameMapper: {
-    '@/(.*)$': '<rootDir>/src/$1',
+  moduleDirectories : [ 'node_modules', '<rootDir>/' ],
+  moduleNameMapper : {
+    '@/(.*)$' : '<rootDir>/src/$1',
   },
-  testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment : 'jest-environment-jsdom',
+  setupFilesAfterEnv : [ '<rootDir>/jest.setup.js' ],
 };
 
 module.exports = createJestConfig(customJestConfig);
