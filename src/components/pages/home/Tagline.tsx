016bfa8e229a6cce7e0d5e4a2cd08043fde147ea
Alfan Jauhari <alfan@zero-one-group.com>
2024-10-22 21:27:11 +0700

    fix: types error

--- src/components/pages/home/Tagline.tsx
+++ src/components/pages/home/Tagline.tsx
@@ -1,24 +1,25 @@
-import { RevealHeading } from "@/components/ui/RevealHeading";
-import { motion, type HTMLMotionProps } from "framer-motion";
-import { forwardRef } from "react";
+import { RevealHeading } from '@/components/ui/RevealHeading'
+import { type HTMLMotionProps } from 'framer-motion'
+import { forwardRef } from 'react'
 
-const TAGLINE = "Architecting Tomorrow's Digital\nLandscape";
+const TAGLINE = "Architecting Tomorrow's Digital\nLandscape"
 
-export const Tagline = forwardRef<HTMLHeadingElement, HTMLMotionProps<"h1">>(
+export const Tagline = forwardRef<HTMLHeadingElement, HTMLMotionProps<'h1'>>(
   ({ className, ...props }, ref) => {
     return (
       <div className="grow flex flex-col gap-2 justify-center">
-        {TAGLINE.split(" ").map((word, index) => (
+        {TAGLINE.split(' ').map((word, index) => (
           <RevealHeading
             key={word}
             className={className}
             sequence={index}
+            ref={ref}
             {...props}
           >
             {word}
           </RevealHeading>
         ))}
       </div>
-    );
-  }
-);
+    )
+  },
+)
--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,13 +1,7 @@
-import clsx from "clsx";
-import { type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import clsx from 'clsx'
+import { type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
-/**
- * Conditionally joins multiple class names together.
- *
- * @param {...ClassValue} classes - variable number of class names
- * @return {string} merged class name string
- */
 export function cn(...classes: ClassValue[]) {
-  return twMerge(clsx(...classes));
+  return twMerge(clsx(...classes))
 }
