66992c2b7121cc5c05154746e9febcd19c04f39c
Alfan Jauhari <alfan@zero-one-group.com>
2024-10-22 17:19:39 +0700

    feat: add blog page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), react()]
+  integrations: [mdx(), tailwind(), react()],
+  markdown: {
+    shikiConfig: {
+      theme: "catppuccin-latte",
+    },
+  },
 });
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,62 +1,22 @@
-'use client'
+"use client";
 
-import { NAVIGATION_MENU } from '@/libs/config'
-import { cn } from '@/libs/utils'
-import {
-  type ComponentProps,
-  forwardRef,
-  useEffect,
-  useImperativeHandle,
-  useRef,
-} from 'react'
+import { NAVIGATION_MENU } from "@/libs/config";
+import { cn } from "@/libs/utils";
+import { type ComponentProps, forwardRef } from "react";
 
-export interface HeaderProps extends ComponentProps<'header'> {
-  pathname: string
+export interface HeaderProps extends ComponentProps<"header"> {
+  pathname: string;
 }
 
 export const Header = forwardRef<HTMLElement, HeaderProps>(
   ({ className, pathname, ...props }, ref) => {
-    const innerRef = useRef<HTMLElement>(null)
-
-    useImperativeHandle(ref, () => innerRef.current!)
-
-    useEffect(() => {
-      let prevScroll = window.scrollY
-
-      const onScroll = () => {
-        const currentScroll = window.scrollY
-
-        if (currentScroll <= prevScroll) {
-          innerRef.current?.classList.remove('-translate-y-full')
-          innerRef.current?.classList.add('translate-y-0')
-        } else {
-          innerRef.current?.classList.remove('translate-y-0')
-          innerRef.current?.classList.add('-translate-y-full')
-        }
-
-        if (currentScroll === 0) {
-          innerRef.current?.classList.remove('bg-white')
-        } else {
-          innerRef.current?.classList.add('bg-white')
-        }
-
-        prevScroll = currentScroll
-      }
-
-      window.addEventListener('scroll', onScroll)
-
-      return () => {
-        window.removeEventListener('scroll', onScroll)
-      }
-    }, [])
-
     return (
       <header
         className={cn(
-          'sticky top-0 z-50 w-full px-6 h-20 flex justify-between items-center mx-auto duration-200',
-          className,
+          "top-0 z-50 w-full px-6 h-20 flex justify-between items-center bg-transparent mx-auto duration-200",
+          className
         )}
-        ref={innerRef}
+        ref={ref}
         {...props}
       >
         <a
@@ -71,7 +31,7 @@ export const Header = forwardRef<HTMLElement, HeaderProps>(
               <li key={menu.href}>
                 <a
                   href={menu.href}
-                  aria-current={pathname === menu.href ? 'page' : undefined}
+                  aria-current={pathname === menu.href ? "page" : undefined}
                   className="text-sm aria-[current]:underline aria-[current]:decoration-dotted text-stone-700"
                 >
                   {menu.name}
@@ -81,8 +41,8 @@ export const Header = forwardRef<HTMLElement, HeaderProps>(
           </ul>
         </nav>
       </header>
-    )
-  },
-)
+    );
+  }
+);
 
-Header.displayName = 'Layout__Header'
+Header.displayName = "Layout__Header";
