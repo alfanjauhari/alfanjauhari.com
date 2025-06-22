c4edfe7cfe92bd97d85ec53b5cca2d2118c06d4b
Alfan Jauhari <hi@alfanjauhari.com>
2025-06-22 20:57:48 +0700

    chore(tailwind): upgrade tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import node from '@astrojs/node'
 import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  integrations: [mdx(), tailwind(), react()],
+  integrations: [mdx(), react()],
 
   markdown: {
     shikiConfig: {
@@ -14,12 +15,19 @@ export default defineConfig({
       wrap: true,
     },
   },
+
   adapter: node({
     mode: 'standalone',
   }),
+
   output: 'server',
   site: process.env.BASE_URL || 'http://localhost:4321',
+
   server: {
     allowedHosts: process.env.NODE_ENV === 'development' ? true : undefined,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 })
