bdbb96ed7aafbbe76872f464a75a52816012f8eb
Alfan Jauhari <hi@alfanjauhari.com>
2025-07-29 17:38:22 +0700

    fix: trusted origins

--- apps/api/src/libs/auth.ts
+++ apps/api/src/libs/auth.ts
@@ -63,5 +63,5 @@ export const auth = betterAuth({
     },
   },
   plugins: [openAPI(), nextCookies()],
-  trustedOrigins: ['http://localhost:4321', 'https://alfanjauhari.com'],
+  trustedOrigins: ['http://localhost:4321', 'https://*.alfanjauhari.com'],
 })
