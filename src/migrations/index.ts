ab8316c161a3d686909fa452289bdf47cb0f9efb
Alfan Jauhari <hi@alfanjauhari.com>
2025-08-17 13:11:29 +0700

    feat: add migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250729_064344 from './20250729_064344'
+import * as migration_20250729_064344 from './20250729_064344';
+import * as migration_20250817_060912 from './20250817_060912';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250729_064344.down,
     name: '20250729_064344',
   },
-]
+  {
+    up: migration_20250817_060912.up,
+    down: migration_20250817_060912.down,
+    name: '20250817_060912'
+  },
+];
