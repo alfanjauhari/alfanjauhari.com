d1f426dd8eaf477dfc10c4e28c366b72d9031e82
Alfan Jauhari <hi@alfanjauhari.com>
2025-08-20 21:09:19 +0700

    feat: add cloudflare purging cache

--- src/collections/Content.ts
+++ src/collections/Content.ts
@@ -3,10 +3,37 @@ import { revalidatePath } from 'next/cache'
 import type { CollectionConfig } from 'payload'
 import { formatSlugHook } from '@/libs/utils'
 
-const revalidateContentPaths = () => {
+const revalidateContentPaths = async () => {
   revalidatePath('/')
   revalidatePath('/updates')
   revalidatePath('/updates/tag/[slug]', 'page')
+
+  if (process.env.CF_DNS_API_TOKEN !== '' && process.env.CF_ZONE_ID !== '') {
+    const response = await fetch(
+      `https://api.cloudflare.com/client/v4/zones/${process.env.CF_ZONE_ID}/purge_cache`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${process.env.CF_DNS_API_TOKEN}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          hosts: ['alfanjauhari.com', 'www.alfanjauhari.com'],
+        }),
+      },
+    )
+
+    if (!response.ok) {
+      console.error(
+        'Error while purging cloudflare cache. No problem!!! \n Error detail:',
+        response.statusText,
+      )
+    } else {
+      console.info('Successfully purging cloudflare cache')
+    }
+  }
+
+  console.info('CACHE PURGED AND REVALIDATED!')
 }
 
 export const Content: CollectionConfig = {
