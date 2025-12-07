bddd392fc1d6ee3bae905c25e9235299dce833cb
Alfan Jauhari <hi@alfanjauhari.com>
2025-12-07 12:56:12 +0700

    feat: ignore .content-colections

--- .content-collections/generated/index.d.ts
+++
@@ -1,7 +0,0 @@
-import configuration from "../../content-collections.ts";
-import { GetTypeByName } from "@content-collections/core";
-
-export type Update = GetTypeByName<typeof configuration, "updates">;
-export declare const allUpdates: Array<Update>;
-
-export {};
