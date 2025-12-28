0e6bf67a8a97f66790b62c9d24c9d0a139b042f3
Alfan Jauhari <hi@alfanjauhari.com>
2025-12-28 20:46:45 +0700

    fix(build): prerender error

--- src/fns/polymorphic/auth.ts
+++ src/fns/polymorphic/auth.ts
@@ -1,8 +1,8 @@
-import { createServerOnlyFn } from "@tanstack/react-start";
+import { createServerFn } from "@tanstack/react-start";
 import { getRequestHeaders } from "@tanstack/react-start/server";
 import { auth } from "@/lib/auth.server";
 
-export const getSessionFn = createServerOnlyFn(async () => {
+export const getSessionFn = createServerFn().handler(async () => {
   const headers = getRequestHeaders();
   const session = await auth.api.getSession({ headers });
 
