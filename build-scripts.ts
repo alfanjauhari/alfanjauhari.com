fc5eb6099c3882296383a9b98c8ac8c062f6ac41
Alfan Jauhari <hi@alfanjauhari.com>
2026-05-19 17:04:04 +0700

    feat: format some files

--- build-scripts.ts
+++ build-scripts.ts
@@ -1,16 +1,13 @@
-import esbuild from 'esbuild';
+import esbuild from "esbuild";
 
 await esbuild.build({
-  entryPoints: ['scripts/db-init.ts', 'scripts/serve.ts'],
-  outdir: 'dist',
-  entryNames: 'scripts/[name]',
+  entryPoints: ["scripts/db-init.ts", "scripts/serve.ts"],
+  outdir: "dist",
+  entryNames: "scripts/[name]",
   bundle: true,
-  packages: 'external',
-  platform: 'node',
-  format: 'esm',
+  packages: "external",
+  platform: "node",
+  format: "esm",
   metafile: true,
-  external: [
-    './dist/server/*',
-    '../server/*',
-  ]
-})
+  external: ["./dist/server/*", "../server/*"],
+});
